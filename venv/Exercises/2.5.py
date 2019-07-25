b38a51cc315a70d842771df44ef376a6ddeea58c
Ksenvolk <Affection5813>
2019-07-25 19:12:42 +0300

    Solution to the ex 2.5

---
+++ venv/Exercises/2.5.py
@@ -0,0 +1,6 @@
+#Write a program which prompts the user for a Celsius temperature,
+# convert the temperature to Fahrenheit, and print out the converted temperature
+
+Cels = input('Input Celsius temperature: ')
+Far = float(Cels) * 33.8
+print('Fahrenheit temperature: ', Far)
